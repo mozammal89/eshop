79e86d9662bb3f5294e215b45e22a3003e62c5f9
mozammal89 <mozammal.bubt89@gmail.com>
2021-03-21 04:27:17 +0600

    Install Vuex

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -15,9 +15,39 @@ import router from './admin/router'
 // Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('admin-master', require('./components/admin/AdminMaster.vue').default);
 
+
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+// this is vuex instance 
+const store = new Vuex.Store({
+    state:{
+        test: "This is test",
+        abc: ''
+    },
+
+    getters:{
+
+    },
+
+    actions:{
+
+    },
+
+    mutations:{
+
+    }
+  })
+
+//   vuex instance end 
+
+
+
 const appadmin = new Vue({
     el: '#appadmin',
     router,
+    store,
     data:{
     }
 });
